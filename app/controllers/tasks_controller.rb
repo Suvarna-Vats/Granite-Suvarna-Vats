cec508b196bc4f37dca962793543ece0c6501d46
Suvarna-Vats <suavarnavats244@gmail.com>
2026-02-11 17:34:12 +0530

    Added API based architecture

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
-    respond_with(@tasks)
+    tasks = Task.all
+    render status: :ok, json: { tasks: }
   end
 end
