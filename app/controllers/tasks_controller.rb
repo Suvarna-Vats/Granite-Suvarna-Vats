9f9513fa5835ebfde2fe616e6a9a835582f83c26
Suvarna-Vats <suavarnavats244@gmail.com>
2026-02-12 18:39:09 +0530

    Added slug to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,10 @@ class TasksController < ApplicationController
     tasks = Task.all
     render status: :ok, json: { tasks: }
   end
+
+  def show
+    task = Task.find_by(slug: params[:slug])
+    render status: :ok, json: { task: } if task
+    render status: :not_found, json: { error: "Task not found" } unless task
+  end
 end
