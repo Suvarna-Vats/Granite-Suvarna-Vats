1290dcc820c357a8b61a2e4f12155ad7047583d2
Suvarna-Vats <suavarnavats244@gmail.com>
2026-02-18 16:48:26 +0530

    Added pundit task policy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,38 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  rescue_from Pundit::NotAuthorizedError, with: :handle_authorization_error
+
   before_action :load_task, only: %i[ show update destroy ]
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
   def index
-    tasks = Task.all.as_json(include: { assigned_user: { only: %i[ id name ] } })
-    render_json({ tasks: })
+    tasks = policy_scope(Task)
+    tasks_with_assigned_user = tasks.as_json(include: { assigned_user: { only: %i[id name email] } })
+    render_json({ tasks: tasks_with_assigned_user })
   end
 
   def show
+    authorize @task
     render
   end
 
   def create
     task = current_user.created_tasks.new(task_params)
+    authorize task
     task.save!
     render_notice(t("successfully_created", entity: "Task"))
   end
 
   def update
+    authorize @task
     @task.update!(task_params)
     render_notice(t("successfully_updated", entity: "Task"))
   end
 
   def destroy
+    authorize @task
     @task.destroy!
     render_notice(t("successfully_deleted", entity: "Task"))
   end
@@ -36,5 +45,9 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit(:title, :assigned_user_id)
+    end
+
+    def handle_authorization_error
+      render_error(t("authorization.denied"), :forbidden)
     end
 end
